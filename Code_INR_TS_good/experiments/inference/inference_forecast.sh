58bb0265e43433d79d0c4ff8805feaf9fb7b2e03
EtienneLnr <92033999+EtienneLnr@users.noreply.github.com>
2023-05-24 14:24:39 +0200

    Delete Code_INR_TS_good directory

--- Code_INR_TS_good/experiments/inference/inference_forecast.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dataset_name=Electricity
-epochs=40000
-horizon=96
-version=0
-test_inner_steps=3
-
-python3 -u inference_forecast.py --dataset_name $dataset_name --epochs $epochs --horizon $horizon --version $version --test_inner_steps $test_inner_steps 
